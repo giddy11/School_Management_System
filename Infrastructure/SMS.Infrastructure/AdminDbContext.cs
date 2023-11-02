289a2dc9073a7c751c14d442b09278c1ab037342
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-11-02 17:16:59 +0100

    added dbcontext

---
+++ Infrastructure/SMS.Infrastructure/AdminDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Infrastructure.Database.Schools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Infrastructure
+{
+    public class AdminDbContext : DbContext
+    {
+        //protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer()
+
+
+        public virtual DbSet<School> Schools { get; set; }
+    }
+}
---
+++ Infrastructure/SMS.Infrastructure/Database/Schools/School.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Infrastructure.Database.Schools
+{
+    public sealed class School
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+    }
+}
